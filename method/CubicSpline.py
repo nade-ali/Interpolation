2b3cf844c526b6d7d75942d8524a5c04b1cbe6d1
nadecho <61347652+nadecho@users.noreply.github.com>
2020-10-06 07:36:19 +0500

    Add files via upload

---
+++ method/CubicSpline.py
@@ -0,0 +1,72 @@
+import numpy as np
+from sympy import*
+
+n = 10
+xinit = np.linspace(-2,2,n)
+xp = np.linspace(-2,2,100)
+yrealf = []
+yp = []
+
+#Create main function for the equation provided in the question
+def mainfunc(x):
+    y = np.exp(x)+2**(-x)+2*(np.cos(x))-6
+    return y
+
+#Declare lists for all the variables required for computing the coefficients
+a = []
+b = []
+c = []
+d = []
+h = []
+alpha = ["nil"]
+
+for i in range(len(xinit)):
+    a.insert(i, mainfunc(xinit[i]))
+
+for i in range(0,n-1):
+    h.insert(i,xinit[i+1]-xinit[i])
+
+for i in range(1,n-1):
+    alpha.insert(i, 3*(((a[i+1]-a[i])/h[i])-((a[i]-a[i-1])/h[i-1])))
+
+i00 = [0]
+z = [0]
+mu = [0]
+
+for i in range(1,n-1):
+    i00.insert(i,2*(xinit[i+1]-xinit[i-1])-((h[i-1])*(mu[i-1])))
+    mu.insert(i,h[i]/i00[i])
+    z.insert(i,(alpha[i]-(h[i-1]*z[i-1]))/i00[i])
+    
+i00.insert(n-1,1)
+z.insert(n-1,0)
+
+for i in range(n):
+    c.insert(i,0)
+    b.insert(i,0)
+    d.insert(i,0)
+
+j = n-2
+while(j>=0):
+    c[j]=z[j]-(mu[j]*c[j+1])
+    b[j]=((a[j+1]-a[j])/h[j])-(h[j]*(c[j+1]+2*c[j]))/3
+    d[j]=(c[j+1]-c[j])/(3*h[j])
+    j = j-1
+
+#Remove last values of each list as they are not used in the calculation
+a.pop(n-1)
+b.pop(n-1)
+c.pop(n-1)
+d.pop(n-1)
+
+for i in range(len(xp)):
+    yrealf.insert(i, mainfunc(xp[i]))
+
+#Cubic Spline Loop
+for i in range(len(xp)):
+    for j in range(n-1):
+        if(xinit[j]<=xp[i] and xp[i]<xinit[j+1]):
+            yp.insert(i,(a[j]+(b[j]*(xp[i]-xinit[j]))+(c[j]*((xp[i]-xinit[j])**2))+(d[j]*((xp[i]-xinit[j])**3))))
+        if(xp[i]==xinit[n-1]):
+            yp.insert(i,(a[n-2]+(b[n-2]*(xp[i]-xinit[n-2]))+(c[n-2]*((xp[i]-xinit[n-2])**2))+(d[n-2]*((xp[i]-xinit[n-2])**3))))
+            break
---
+++ src/Polynomial.py
@@ -0,0 +1,36 @@
+import matplotlib
+import matplotlib.pyplot as mplot
+import Lagrange as LagrangeInterpolation
+import CubicSpline as CubicSpline
+
+figure, bpo = mplot.subplots()
+
+#Main function with actual values
+mplot.subplot(2, 2, 1)
+mplot.plot(CubicSpline.xp, CubicSpline.yrealf, 'k-')
+mplot.plot(LagrangeInterpolation.xinit, LagrangeInterpolation.yinit, 'ko')
+mplot.title('Function (Dots are initial points)')
+
+#Cubic Spline Graph
+mplot.subplot(2, 2, 2)
+mplot.plot(CubicSpline.xp, CubicSpline.yp, 'r-')
+mplot.title('Cubic Spline')
+
+#Lagrange Graph
+mplot.subplot(2, 2, 3)
+mplot.plot(LagrangeInterpolation.xp, LagrangeInterpolation.yp, 'y-')
+mplot.title('Lagrange Interpolation')
+
+#All graphs combined
+mplot.subplot(2, 2, 4)
+mplot.plot(LagrangeInterpolation.xp, LagrangeInterpolation.yp, 'y-')
+mplot.plot(CubicSpline.xp, CubicSpline.yp, 'r-')
+mplot.plot(CubicSpline.xp, CubicSpline.yrealf, 'k-')
+mplot.plot(LagrangeInterpolation.xinit, LagrangeInterpolation.yinit, 'ko')
+mplot.title('Comparison')
+
+#Display the graph 
+mplot.tight_layout()
+figure.savefig("picture.png", bbox_inches="tight", dpi=300)
+mplot.show()
+
